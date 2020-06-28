9e4f4dafcc1f33855b151e840df799ab4669fd71
German Vives Hernandez <gvives@outlook.com>
2020-06-27 23:41:58 -0600

    Functional comparaison from Key and subkey

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,7 +4,10 @@ using System.Data;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Converters;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml;
 using Registry;
 using Registry.Abstractions;
 
@@ -157,7 +160,17 @@ namespace RegistryToolbox
                 
                 string path = GetFullPath(item);
                 if (!this.actualpath1.Contains(path))
+                {
                     loadtable(GetFullPath(item), 1);
+
+                    if (item.Background.ToString() != "#00FFFFFF")
+                    {
+                        Reg1Values.UpdateLayout();
+                        Reg2Values.UpdateLayout();
+                        Paint_Differences(Reg2Values, Reg1Values);
+                        Paint_Differences(Reg1Values, Reg2Values);
+                    }
+                }
                 this.actualpath1 = path;
             }
             else
@@ -167,7 +180,16 @@ namespace RegistryToolbox
                 TreeViewItem item = sender as TreeViewItem;
                 string path = GetFullPath(item);
                 if (!this.actualpath2.Contains(path))
+                {
                     loadtable(GetFullPath(item), 2);
+                    if (item.Background != Brushes.White)
+                    {
+                        Reg1Values.UpdateLayout();
+                        Reg2Values.UpdateLayout();
+                        Paint_Differences(Reg2Values, Reg1Values);
+                        Paint_Differences(Reg1Values, Reg2Values);
+                    }
+                }
                 this.actualpath2 = path;
 
             }
@@ -492,14 +514,146 @@ namespace RegistryToolbox
             btnload2_Click(null,null);
             btnload1_Click(null, null);
         }
+        private void PaintDifferencesTree(TreeViewItem pNode1, TreeViewItem pNode2)
+        {
+            if (pNode1 == null)
+            {
+                if (pNode2 != null)
+                {
+                    pNode2.Background = new SolidColorBrush(Color.FromRgb(224, 255, 255)); //CYAN
+                }
+            }
+            else
+            {
+                if (pNode2 == null)
+                {
+                    pNode1.Background = new SolidColorBrush(Color.FromRgb(224, 255, 255)); //CYAN
+                }
+                else // BOTH ARE NOT NULL THEY HAVE VALID
+                {
+                    string path1 = GetFullPath(pNode1);
+                    string path2 = GetFullPath(pNode2);
+                    RegistryKey keyA = Registry1.GetKey(path1);
+                    RegistryKey keyB = Registry2.GetKey(path2);
+                    if (!Compare_KeyEq(keyA, keyB))
+                    {
+                        {
+                            pNode1.Background = new SolidColorBrush(Color.FromRgb(255, 204, 203)); //LIGHT RED
+                            pNode2.Background = new SolidColorBrush(Color.FromRgb(255, 204, 203)); //LIGHT RED
+                        }
+                    }
+                    foreach (TreeViewItem item1 in pNode1.Items)
+                    {
+                        foreach (TreeViewItem item2 in pNode2.Items)
+                        {
+                            if (item1.Header.ToString() == item2.Header.ToString())
+                            {
 
+                                PaintDifferencesTree(item1, item2);
+                           
+                            }
+
+                        }
+                    }
+                }
+
+
+            }
+        }
         private void btnCompareKeyandsub_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine(Registry1.GetKey(txtpath1.Text).ToString());
-        }
-        private void Compare_Structure()
-        {
+            //Debug.WriteLine(Registry1.GetKey(txtpath1.Text).ToString());
+         
+            TreeViewItem node1 = (TreeViewItem)Reg1Tree.SelectedItem;
+            TreeViewItem node2 = (TreeViewItem)Reg2Tree.SelectedItem;
+            PaintDifferencesTree(node1, node2);
 
+            //if (!Compare_KeyAndSubkeys(keyA, keyB))
+            //{
+            //    this.Reg2Tree.Items.Add(guiNodechild);
+            //    Drawhive(subkey, guiNodechild, registryfile);
+            //    MessageBox.Show("ERROR", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
+
+        }
+        private bool Compare_RegkeyEq(KeyValue pvalues1 , KeyValue pvalues2)
+        {
+            bool nameeq = pvalues1.ValueName == pvalues2.ValueName;
+            bool typeeq = pvalues1.ValueType == pvalues2.ValueType;
+            bool valueeq = pvalues1.ValueData == pvalues2.ValueData;
+
+            return (nameeq & typeeq & valueeq);
+        }
+        private bool Compare_KeyAndSubkeys(RegistryKey pValues1, RegistryKey pValues2)
+        {
+            if (pValues1.SubKeys.Count == 0)
+            {
+                if (pValues2.SubKeys.Count == 0)
+                {
+                    return Compare_KeyEq(pValues1, pValues2);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (pValues2.SubKeys.Count == 0)
+                {
+                    return false;
+                }
+                {
+                    //this case is both trees have leafs
+                    Boolean equals = true;
+                    foreach (RegistryKey subkey1 in pValues1.SubKeys)
+                    {
+                        Boolean found = false;
+                        foreach (RegistryKey subkey2 in pValues2.SubKeys)
+                        {
+                            if (subkey1.KeyName == subkey2.KeyName)
+                            {
+                                equals = equals & Compare_KeyAndSubkeys(subkey1, subkey2);
+                                found = true;
+                            }
+                        }
+                        if (found == false) //This is differente because we found a key with different name
+                        {
+                            return false;
+                        }
+                        if (!equals) // If equals is false we found a difference
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+                return true;
+            }
+        }
+        
+        private bool Compare_KeyEq(RegistryKey pValues1, RegistryKey pValues2)
+        {
+            if (pValues1.Values.Count != pValues2.Values.Count) //If different number of values we found differences
+            {
+                return false;
+            }
+            else
+            {
+                foreach(KeyValue key1 in pValues1.Values)
+                {
+                    foreach(KeyValue key2 in pValues2.Values)
+                    {
+                        if (key1.ValueName == key2.ValueName)
+                        {
+                            if (!Compare_RegkeyEq(key1, key2))
+                                return false;
+                        }
+                    }
+                }
+            }
+            return true;
         }
     }
 }
